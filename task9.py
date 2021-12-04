996faeac3cfa66d55c39ef277297813f50735e06
Sarunas Laurinavicius <sarunas856@gmail.com>
2021-12-04 20:56:04 +0200

    first commit

---
+++ task9.py
@@ -0,0 +1,25 @@
+import functools
+
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug
+
+
+@debug
+def func(a, b, c):
+    return a + b * c
+
+
+if __name__ == '__main__':
+    print(func(a=3, b=2, c=4))
+    
